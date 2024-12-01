d282eb06144c6c70932ec370fb63f0d3744092b4
quythuong <quythuong0108@gmail.com>
2024-12-01 12:18:51 +0700

    Upload the video frame as a thumbnail to cloudinary if there is no thumbnail upload, this step isafter video processing

--- src/main/java/com/qt/VideoPlatformAPI/File/VideoFileProcessingService.java
+++ src/main/java/com/qt/VideoPlatformAPI/File/VideoFileProcessingService.java
@@ -1,10 +1,13 @@
 package com.qt.VideoPlatformAPI.File;
 
+import com.qt.VideoPlatformAPI.Config.VideoEnv;
 import com.qt.VideoPlatformAPI.Video.Video;
 import com.qt.VideoPlatformAPI.Video.VideoService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
@@ -25,8 +28,18 @@ public class VideoFileProcessingService {
         }).thenAccept((res) -> {
             videoService.updateVideoProcessedStatus(videoId);
             Video v = videoService.getVideoById(videoId);
-//            if(v.getThumbnailUrl() == null || v.getThumbnailUrl().isBlank())
-//                videoService.updateThumbnailVideo(videoId, )
+            if(v.getThumbnailUrl() == null || v.getThumbnailUrl().isBlank()) {
+                String filePath = VideoEnv.ROOT_LOCATION + "/" + videoId + "/thumbnail.jpg";
+                File file = new File(filePath); // Locate the thumbnail file
+                if (!file.exists()) {
+                    throw new IllegalArgumentException("File not found at path: " + filePath);
+                }
+                try {
+                    videoService.updateThumbnailVideo(videoId, (MultipartFile) file);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         });
     }
 }
